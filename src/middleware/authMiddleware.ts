d9c3fd52119e434fbe0e94913d2899c19ce305e5
Andy Rabeson <hei.andy.19@gmail.com>
2026-08-16 11:30:12 +0300

    Making all errors to english

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -7,12 +7,12 @@ export const authenticateToken = (req: AuthRequest, res: Response, next: NextFun
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1]; 
   if (!token) {
-    return res.status(401).json({ error: "Token manquant" });
+    return res.status(401).json({ error: "Token missing" });
   }
   
   jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
     if (err) {
-      return res.status(403).json({ error: "Token invalide ou expiré" });
+      return res.status(403).json({ error: "Token invalid or expired" });
     }
     
     req.user = user;
