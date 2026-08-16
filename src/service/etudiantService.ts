9d673b886fea5ae96ab72489a436afa82207c1e3
Andy Rabeson <hei.andy.19@gmail.com>
2026-08-16 08:01:10 +0300

    Modification of all the function in etudiantService.ts to anonymous

--- src/service/etudiantService.ts
+++ src/service/etudiantService.ts
@@ -1,22 +1,22 @@
 import { findAll, findName, insertEtudiant, deleteEtudiant, updateEtudiant } from "../repository/etudiantRepository";
 import { Etudiant } from "../models/etudiantModels";
 
-export async function getAllEtudiants(): Promise<Etudiant[]> {
+export const getAllEtudiants = async (): Promise<Etudiant[]> => {
   return await findAll();
 }
 
-export async function getAllNames(): Promise<string[]> {
+export const getAllNames= async(): Promise<string[]> => {
   return await findName();
 }
 
-export async function createStudent(etudiant: Omit<Etudiant, "id">): Promise<Etudiant> {
+export const createStudent= async(etudiant: Omit<Etudiant, "id">): Promise<Etudiant> => {
   return await insertEtudiant(etudiant);
 }
 
-export async function deleteStudent(id: number): Promise<void> {
+export const deleteStudent = async(id: number): Promise<void> =>{
   return await deleteEtudiant(id);
 }
 
-export async function updateStudent(id: number, etudiant: Partial<Etudiant>): Promise<Etudiant | null> {
+export const updateStudent = async(id: number, etudiant: Partial<Etudiant>): Promise<Etudiant | null> =>{
   return await updateEtudiant(id, etudiant);
 }
